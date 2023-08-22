23eec688a56ef010bece45d9d7d1311d5af4ef28
Kpgdh <kpgdh.gsjvsh@gmail.com>
2023-08-22 17:17:55 +0300

    File recovery

---
+++ main.java
@@ -0,0 +1,13 @@
+package TestGIT;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+
+        System.out.println("Nice to meet you");
+
+        System.out.println("Whats your name?");
+        System.out.println("How old are you?");
+
+    }
+}
